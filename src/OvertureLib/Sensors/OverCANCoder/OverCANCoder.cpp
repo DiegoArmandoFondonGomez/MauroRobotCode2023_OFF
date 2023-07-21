d36107e8bd32dbe0f0531748dd7f484c95af8a84
Luis Leyva <a01570367@itesm.mx>
2023-07-21 17:45:32 -0600

    Posible Fix

--- src/OvertureLib/Sensors/OverCANCoder/OverCANCoder.cpp
+++ src/OvertureLib/Sensors/OverCANCoder/OverCANCoder.cpp
@@ -5,14 +5,13 @@
 #include "OverCANCoder.h"
 
 OverCANCoder::OverCANCoder(int _id, double offset, std::string _bus) {
-    canCoder = new CANcoder(_id, _bus);
-    canCoderConfiguration.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue::Signed_PlusMinusHalf;
-    canCoderConfiguration.MagnetSensor.SensorDirection = SensorDirectionValue::CounterClockwise_Positive;
-    canCoderConfiguration.MagnetSensor.MagnetOffset = offset / 360;
-    canCoder->GetConfigurator().Apply(canCoderConfiguration);
+	canCoder = new CANcoder(_id, _bus);
+	canCoderConfiguration.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue::Signed_PlusMinusHalf;
+	canCoderConfiguration.MagnetSensor.SensorDirection = SensorDirectionValue::CounterClockwise_Positive;
+	canCoderConfiguration.MagnetSensor.MagnetOffset = offset / 360;
+	canCoder->GetConfigurator().Apply(canCoderConfiguration);
 }
 
 double OverCANCoder::getAbsolutePosition() {
-    canCoder->GetAbsolutePosition().SetUpdateFrequency(200_Hz);
-    return canCoder->GetAbsolutePosition().Refresh().GetValue().value();
+	return canCoder->GetAbsolutePosition().Refresh().GetValue().value();
 }
