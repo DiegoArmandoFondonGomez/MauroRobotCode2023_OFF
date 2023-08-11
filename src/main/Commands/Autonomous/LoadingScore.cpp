3e89ef9aeb98962f8d49bfcf0439328156f16ad0
DiegoArmandoFondon <a01199394@tec.mx>
2023-08-10 18:14:13 -0600

    typos (jaja)

--- src/main/Commands/Autonomous/LoadingScore.cpp
+++ src/main/Commands/Autonomous/LoadingScore.cpp
@@ -1,8 +1,8 @@
 #include "Autonomous.h"
 
-frc2::CommandPtr LoadingSocre(SuperStructure* m_SuperStructure, Intake* m_Intake, Chassis* m_Chassis) {
+frc2::CommandPtr LoadingScore(SuperStructure* m_SuperStructure, Intake* m_Intake, Chassis* m_Chassis) {
     //Get Alliance Color
-    frc::DriverStation::Aliance allianceColor = frc::DriverStation::GetAlliance();
+    frc::DriverStation::Alliance allianceColor = frc::DriverStation::GetAlliance();
 
     //Get Trajectories
     pathplanner::PathPlannerTrajectory trajectoryOne = pathplanner::PathPlanner::loadPath("LoadingFirstPiece", { 2_mps, 2_mps_sq });
@@ -13,7 +13,7 @@ frc2::CommandPtr LoadingSocre(SuperStructure* m_SuperStructure, Intake* m_Intake
     return frc2::cmd::Sequence(
 
         frc2::InstantCommand([m_Chassis, initialPose]() {m_Chassis->resetOdometry({ initialPose.X(), initialPose.Y(), 180_deg });}).ToPtr(),
-        SwerveTrajectories(m_Chassis, trajectoryOne, { 1,0,0 }, { -1,0,0 }, { 1.27,0,0 }).asProxy()
+        SwerveTrajectories(m_Chassis, trajectoryOne, { 1,0,0 }, { -1,0,0 }, { 1.27,0,0 }).AsProxy()
 
     );
 }
