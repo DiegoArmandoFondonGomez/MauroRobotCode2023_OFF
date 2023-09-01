5bda5f5c3a27ef55030fd2549b80da300d272c27
Esquivel28 <109557909+Esquivel28@users.noreply.github.com>
2023-08-31 18:04:59 -0600

    BarrierScore(Autonomous)🐳

---
+++ src/main/Commands/Autonomous/BarrierScore.h
@@ -0,0 +1,33 @@
+#include "Autonomous.h"
+
+frc2::CommandPtr BarrierScore(SuperStructure* m_SuperStructure, Intake* m_Intake, Chassis* m_Chassis) {
+    //Get Alliance Color
+    frc::DriverStation::Alliance allianceColor = frc::DriverStation::GetAlliance();
+
+    //Get Trajectories
+    pathplanner::PathPlannerTrajectory BarrierFirstPiece = pathplanner::PathPlanner::loadPath("BarrierFirstPiece", { 3.5_mps, 3.5_mps_sq });
+
+    //Get Initial Pose
+    frc::Pose2d initialPose = pathplanner::PathPlannerTrajectory::transformTrajectoryForAlliance(BarrierFirstPiece, allianceColor).getInitialPose();
+
+    return frc2::cmd::Sequence(
+
+        UpperCommand(m_SuperStructure),
+        SetGamePieceTrueCommand(m_Intake, 6.0_V),
+        frc2::WaitCommand(0.5_s),
+        SetGamePieceFalseCommand(m_Intake),
+        ClosedCommand(m_SuperStructure),
+
+        frc2::cmd::Parallel(
+            SwerveTrajectories(m_Chassis, BarrierFirstPiece, { 1,0,0 }, { -1,0,0 }, { 1.27,0,0 }).AsProxy(),
+
+            frc2::cmd::Sequence(
+                frc2::WaitCommand(2.35_s),
+                GroundIntakeTrueCommand(m_SuperStructure, m_Intake, -6.0_V),
+                frc2::WaitCommand(1.5_s),
+                GroundIntakeFalseCommand(m_SuperStructure, m_Intake)
+            )
+        )
+
+    );
+};
