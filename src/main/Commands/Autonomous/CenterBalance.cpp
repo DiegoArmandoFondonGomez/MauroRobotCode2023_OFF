a2137dff395ab6af81da8afe740161cfb0bf5fe5
Luis Leyva <luisoleyvan@gmail.com>
2023-09-05 10:01:40 -0600

    Auto Change

--- src/main/Commands/Autonomous/CenterBalance.cpp
+++ src/main/Commands/Autonomous/CenterBalance.cpp
@@ -5,7 +5,7 @@ frc2::CommandPtr CenterBalance(SuperStructure* m_SuperStructure, Intake* m_Intak
 	frc::DriverStation::Alliance allianceColor = frc::DriverStation::GetAlliance();
 
 	//Get Trajectories
-	pathplanner::PathPlannerTrajectory CenterBalance = pathplanner::PathPlanner::loadPath("CenterBalance", { 3.5_mps, 3.5_mps_sq });
+	pathplanner::PathPlannerTrajectory CenterBalance = pathplanner::PathPlanner::loadPath("CenterBalance", { 2_mps, 2_mps_sq });
 
 	// Transform Trajectories for Alliance Color
 	CenterBalance = pathplanner::PathPlannerTrajectory::transformTrajectoryForAlliance(CenterBalance, allianceColor);
